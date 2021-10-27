c926d13e1f2ec33d0ea09cb4d2d81d0438c1ecc7
Алексей Лысов <lexazombike72@gmail.com>
2021-10-27 20:56:56 +0300

    Добавьте файлы проекта.

---
+++ OTIK_Encoder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OTIK_Encoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
