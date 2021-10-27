97f4667a2416716ca927e785e988a5c887da24a3
dimitryku <diman.kuzmin347@gmail.com>
2021-10-27 23:30:22 +0300

    added file path

---
+++ OTIK_Encoder/FileHolder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTIK_Encoder
+{
+    class FileHolder
+    {
+        private string _name;
+        private List<byte> _data;
+
+        public FileHolder(string name, in List<byte> data)
+        {
+            _name = name;
+            _data = data;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public List<byte> GetData()
+        {
+            return _data;
+        }
+
+        public uint getDataLength()
+        {
+            return (uint)_data.Count;
+        }
+
+        public ushort getNameLength()
+        {
+            return (ushort)_name.Length;
+        }
+
+    }
+}
