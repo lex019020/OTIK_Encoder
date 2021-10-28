839887c6df26e49e8874a7e117e74baeb91e1fee
Алексей Лысов <lexazombike72@gmail.com>
2021-10-28 16:11:29 +0300

    Implemented file handling struct

---
+++ OTIK_Encoder/FileHandlingStruct.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTIK_Encoder
+{
+    struct FileHandlingStruct
+    {
+        public bool randomSplit_1;
+        public List<byte> bytes;
+    }
+}
