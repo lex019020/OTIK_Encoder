152127e1a6ae84335624bb4549f3ee354bd9acd8
dimitryku <diman.kuzmin347@gmail.com>
2021-10-28 19:09:50 +0300

    added loaders and savers TODO implement

---
+++ OTIK_Encoder/FileSaver.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTIK_Encoder
+{
+    class FileSaver
+    {
+        private readonly string path;
+
+        public FileSaver(string path) 
+        {
+            
+        }
+
+        public void AddFile(string name, List<byte> bytes) 
+        {
+            
+        }
+    }
+}
