884afadab26da6220d8dd08f600323bdd0c674e1
Алексей Лысов <lexazombike72@gmail.com>
2021-10-28 16:42:52 +0300

    Small fix for ArchiveHeader, started implementing ArchiveProcessor

---
+++ OTIK_Encoder/ArchiveProcessor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTIK_Encoder
+{
+    class ArchiveProcessor
+    {
+        public static void Encode(RandSplitType rSplitting, EntropicBasedCompressionType entCompr, 
+            ContextBasedCompressionType cbCompr, AntiInterferenceType antiInterf, 
+            string input, string output)
+        {
+
+        }
+    }
+}
